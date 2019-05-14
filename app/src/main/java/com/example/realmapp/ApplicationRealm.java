139e3fc566bcec86be5a955b460785d4f920ec80
Dani Pando Moreno <34524302+RedWolf3121@users.noreply.github.com>
2019-05-14 17:46:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/realmapp/ApplicationRealm.java
@@ -0,0 +1,27 @@
+package com.example.realmapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class ApplicationRealm extends Application {
+
+    //configuracion del realm
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        setupRealmConfig();
+        Realm realm = Realm.getDefaultInstance();
+        realm.close();
+    }
+    private void setupRealmConfig(){
+        Realm.init(getApplicationContext());
+        RealmConfiguration config = new RealmConfiguration
+                .Builder()
+                .schemaVersion(1)
+                .migration(new RealmMigration())
+                .build();
+        Realm.setDefaultConfiguration(config);
+    }
+}
---
+++ app/src/main/java/com/example/realmapp/Contact.java
@@ -0,0 +1,85 @@
+package com.example.realmapp;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
+public class
+Contact extends RealmObject {
+    @PrimaryKey
+    private long id;
+
+//    @Required
+//    private String name;
+
+    @Required
+    private String nameNumber;
+
+//    @Required
+//    private String numero;
+    private int edad;
+    private boolean genero;
+
+    public Contact(){}
+    public Contact(long idExt, String nameExt, String numberExt){
+        id = idExt;
+//        name = nameExt;
+//        numero = numberExt;
+    }
+    public Contact(long idExt, String nameExt, int edadExt, boolean generoExt){
+        id = idExt;
+        nameNumber = nameExt;
+//        numero = numberExt;
+        edad = edadExt;
+        genero = generoExt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getNumero() {
+//        return numero;
+//    }
+//
+//    public void setNumero(String numero) {
+//        this.numero = numero;
+//    }
+
+    public String getNameNumber() {
+        return nameNumber;
+    }
+
+    public void setNameNumber(String nameNumber) {
+        this.nameNumber = nameNumber;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isGenero() {
+        return genero;
+    }
+
+    public void setGenero(boolean genero) {
+        this.genero = genero;
+    }
+}
+
